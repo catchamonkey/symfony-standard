8a966b6486d43dde3fbf87dff7d097130e848145
Chris Sedlmayr <chris@sedlmayr.co.uk>
2013-02-28 14:11:14 +0000

    To ease the explanation, i've added a logger error call when the secondaryMethod is called

--- src/Acme/DemoBundle/EventListener/SecondaryListener.php
+++ src/Acme/DemoBundle/EventListener/SecondaryListener.php
@@ -4,8 +4,16 @@ namespace Acme\DemoBundle\EventListener;
 
 class SecondaryListener
 {
+    protected $logger;
+
+    public function __construct($logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function secondaryMethod()
     {
+        $this->logger->err('SecondaryListener::secondaryMethod called');
         // die('SecondaryListener::secondaryMethod called');
     }
 }
